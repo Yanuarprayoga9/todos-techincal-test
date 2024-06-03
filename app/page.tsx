a482601e1570313d0a668c70d7a7c6a71b8ed32e
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-06-03 18:46:21 +0700

    chore: instal & setup prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { prismadb } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const user = await prismadb.category.findMany();
+  console.log({user})
   return (
     <Button >hello world</Button>
   );
