aa9aa5f9ae17313a6c6ab27775daa32e857ca047
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-06-04 05:41:39 +0700

    chore: delete category type

--- types.ts
+++ types.ts
@@ -1,19 +1,11 @@
-export interface Category {
-    id: string;
-    name: string;
-    createdAt: Date;
-    updatedAt: Date;
-    todos?: Todo[];
-  }
+
   
   export interface Todo {
     id: string;
     name: string;
     value: string;
     isCompleted: boolean;
-    categoryId: string;
     createdAt: Date;
     updatedAt: Date;
-    category: Category;
   }
   
