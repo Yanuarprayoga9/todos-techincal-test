e276ee33bfd9e403ed764e01227daebb2dc4fc7e
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-06-04 08:50:43 +0700

    feat: ui todos list sample

--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -1,8 +1,53 @@
+import { BellRing, Check } from "lucide-react";
 
-const TodoItem = ({}) => {
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+type CardProps = React.ComponentProps<typeof Card>;
+
+export function TodoItem({ className, ...props }: CardProps) {
   return (
-    <div>TodoItem</div>
-  )
+    <Card className={cn("w-[420px] sm:w-[380px]", className)} {...props}>
+      <CardHeader>
+        <CardTitle>Notifications</CardTitle>
+        <CardDescription>17-18-2000.</CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className=" flex items-center space-x-4 rounded-md border p-4">
+          <BellRing />
+          <div className="flex-1 space-y-1">
+            <p className="text-sm font-medium leading-none">
+              Push Notifications
+            </p>
+            {/* <p className="text-sm text-muted-foreground">
+              Send notifications to device.
+            </p> */}
+          </div>
+        </div>
+        {/* <div>
+          <div className="flex-1 space-y-1">
+            <p className="text-sm font-medium leading-none">
+              Push Notifications
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Send notifications to device.
+            </p>
+          </div>
+        </div> */}
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">
+          <Check className="mr-2 h-4 w-4" /> Mark as complete
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 }
-
-export default TodoItem
--- components/todo-list.tsx
+++ components/todo-list.tsx
@@ -1,7 +1,14 @@
+import { TodoItem } from "./todo-item"
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
+    <div className="flex flex-wrap justify-center gap-4">
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+    </div>
   )
 }
 
