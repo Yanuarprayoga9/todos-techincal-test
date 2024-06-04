3496b09e08d4cb14b2080c704220b11f14567973
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-06-04 14:14:51 +0700

    chore: date format function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function formattedDate(p: string): string {
+  const date = new Date(p);
+  return format(date, "hh:mm a dd-MM-yyyy"); // You can adjust the format as needed
 }
