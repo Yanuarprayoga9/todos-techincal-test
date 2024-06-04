bece813bcf95382f197d9f6f2f702e128ef8631b
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-06-05 04:46:35 +0700

    fix: validate queries all todos

--- hooks/todo/use-delete-todo.tsx
+++ hooks/todo/use-delete-todo.tsx
@@ -1,23 +1,25 @@
 import { baseUrl } from "@/lib/baseUrl";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const URL = `${baseUrl}`; // Adjust the URL as needed
+const URL = `${baseUrl}/`; // Adjust the URL as needed
 
-type OnSuccessFn = (data: { id: string }) => void;
+export const useDeleteTodo = () => {
+  const queryClient = useQueryClient();
 
-export const useDeleteTodo = ({ onSuccess }: { onSuccess: OnSuccessFn }) => {
   return useMutation({
     mutationFn: async (id: string): Promise<{ id: string }> => {
-      const res = await fetch(`${URL}/${id}`, {
+      console.log({id})
+      const res = await fetch(`${URL}${id}`, {
         method: "DELETE",
       });
 
       if (!res.ok) {
         throw new Error("Failed to delete todo");
       }
-
       return { id };
     },
-    onSuccess,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
   });
 };
