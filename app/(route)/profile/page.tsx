3c2925f8173a8510ab1f0980f05616c7daf9e2e6
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-06-05 11:30:17 +0700

    feat: route name page

--- app/(route)/profile/page.tsx
+++ app/(route)/profile/page.tsx
@@ -2,7 +2,7 @@ import Container from "@/components/ui/container";
 import React from "react";
 
 const page = () => {
-  return <Container>page</Container>;
+  return <Container>profile</Container>;
 };
 
 export default page;
